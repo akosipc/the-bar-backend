7bc8fd3c895fd0f676e7e33eab789544482baeb7
Adrian Peterson Co <adrianpco@gmail.com>
2017-06-01 14:53:32 +0800

    Whoops!

--- config/prod.exs
+++ config/prod.exs
@@ -20,7 +20,7 @@ config :thebar, Thebar.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
-config :sweaty, Thebar.Repo,
+config :thebar, Thebar.Repo,
   adapter: Ecto.Adapters.Postgres,
     url: System.get_env("DATABASE_URL"),
     pool_size: 15

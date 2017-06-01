189ea55069cacd405c65fd863fbe0867dbec4555
Adrian Peterson Co <adrianpco@gmail.com>
2017-06-01 14:40:19 +0800

    Finishes the whole app lol

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,13 @@
 defmodule Thebar.PageController do
   use Thebar.Web, :controller
 
-  def index(conn, _params) do
-    render conn, "index.html"
+  alias Thebar.{ Ingredient, Recipe, Drink }
+
+  def index(conn, _) do
+    category_ids = Repo.all(from i in Ingredient, where: i.available == true, select: i.category_id)
+    drink_ids = Repo.all(from r in Recipe, where: r.category_id in ^category_ids, select: r.drink_id)
+    drinks = Repo.all(from d in Drink, where: d.id in ^drink_ids)
+
+    render conn, "index.html", drinks: drinks
   end
 end

109f2be0e065dd1161316fb5294a4c31105b11a4
Adrian Peterson Co <adrianpetersonco@gmail.com>
2019-03-27 14:16:47 +0800

    Removes the queries for now

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,10 +4,12 @@ defmodule Thebar.PageController do
   alias Thebar.{ Ingredient, Recipe, Drink }
 
   def index(conn, _) do
-    category_ids = Repo.all(from i in Ingredient, where: i.available == true, select: i.category_id)
-    drink_ids = Repo.all(from r in Recipe, where: r.category_id in ^category_ids, select: r.drink_id)
-    drinks = Repo.all(from d in Drink, where: d.id in ^drink_ids)
+    #category_ids = Repo.all(from i in Ingredient, where: i.available == true, select: i.category_id)
+    #drink_ids = Repo.all(from r in Recipe, where: r.category_id in ^category_ids, select: r.drink_id)
+    #drinks = Repo.all(from d in Drink, where: d.id in ^drink_ids)
 
-    render conn, "index.html", drinks: drinks
+    #render conn, "index.html", drinks: drinks
+    
+    render conn, "index.html"
   end
 end
